cd965a830aaf8582aaad37de270aeaa9611820e9
DanielDziubecki <daniel.dziubecki@wp.pl>
2019-11-22 09:33:33 +0100

    push to repo script

---
+++ pushToRepo.ps1
@@ -0,0 +1,8 @@
+﻿$file = Get-ChildItem *.tgz
+$found = $file -match '.*-(\d+(\.\d+){1,3}).*'
+if ($found) {
+    $version = $matches[1]
+    $fullname = "itdepends.$($version).tgz"
+    az acr helm repo add --name itdepends
+    az acr helm push $fullname 
+}
