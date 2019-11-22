26be73eb1bbbad2f280cfeb96dd90cdf7ef924bb
DanielDziubecki <daniel.dziubecki@wp.pl>
2019-11-22 20:51:02 +0100

    login

--- pushToRepo.ps1
+++ pushToRepo.ps1
@@ -3,6 +3,7 @@ $found = $file -match '.*-(\d+(\.\d+){1,3}).*'
 if ($found) {
     $version = $matches[1]
     $fullname = "itdepends-$($version).tgz"
-    az acr helm repo add --name itdepends
+    az login --service-principal -u $env:SERVICEPRINCIPALID -p $env:SERVICEPRINCIPALKEY --tenant $env:SERVICEPRINCIPALTENANTID
+	az acr helm repo add --name itdepends
     az acr helm push --name itdepends $fullname 
 }
