5a19170118b216cca7970c747af08354234a99d9
DanielDziubecki <daniel.dziubecki@wp.pl>
2019-11-22 09:42:59 +0100

    add -

--- pushToRepo.ps1
+++ pushToRepo.ps1
@@ -2,7 +2,7 @@
 $found = $file -match '.*-(\d+(\.\d+){1,3}).*'
 if ($found) {
     $version = $matches[1]
-    $fullname = "itdepends.$($version).tgz"
+    $fullname = "itdepends-$($version).tgz"
     az acr helm repo add --name itdepends
     az acr helm push $fullname 
 }
