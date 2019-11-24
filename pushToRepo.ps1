2e50c0b8af0876ca402475a280d86d89e102d2e8
DanielDziubecki <daniel.dziubecki@wp.pl>
2019-11-24 08:35:58 +0100

    cleanup

--- pushToRepo.ps1
+++ pushToRepo.ps1
@@ -1,9 +1,4 @@
 ﻿$file = Get-ChildItem *.tgz
-$found = $file -match '.*-(\d+(\.\d+){1,3}).*'
-if ($found) {
-    $version = $matches[1]
-    $fullname = "itdepends-$($version).tgz"
-    az login --service-principal -u $env:SERVICEPRINCIPALID -p $env:SERVICEPRINCIPALKEY --tenant $env:SERVICEPRINCIPALTENANTID
-	az acr helm repo add --name itdepends
-    az acr helm push --name itdepends $fullname 
-}
+az login --service-principal -u $env:SERVICEPRINCIPALID -p $env:SERVICEPRINCIPALKEY --tenant $env:SERVICEPRINCIPALTENANTID
+az acr helm repo add --name itdepends
+az acr helm push --name itdepends $file.Name
