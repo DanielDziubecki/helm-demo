120931b38214278fcc02be11deb8dca6e1d70e55
DanielDziubecki <daniel.dziubecki@wp.pl>
2019-12-04 07:54:47 +0100

    cleanup

--- pushToRepo.ps1
+++
@@ -1,4 +0,0 @@
-﻿$file = Get-ChildItem *.tgz
-az login --service-principal -u $env:SERVICEPRINCIPALID -p $env:SERVICEPRINCIPALKEY --tenant $env:SERVICEPRINCIPALTENANTID
-az acr helm repo add --name itdepends
-az acr helm push --name itdepends $file.Name
